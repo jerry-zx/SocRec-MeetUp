f710b1db414b1f28ad10f2ec99626cb219ee8627
waitzxzx <forzhuxi@outlook.com>
2021-05-10 00:02:59 +0800

    model updated

--- model.py
+++ model.py
@@ -10,7 +10,13 @@ class DNN(nn.Module):
         self.g_embedding = nn.Embedding(args.n_group, args.embed_dim)
         self.t_embedding = nn.Embedding(args.n_topic, args.embed_dim)
 
-        self.fc1 = nn.Linear(args.embed_dim * 5, args.hidden_size)
+        self.user_mlp_1 = nn.Linear(args.embed_dim * 2, args.embed_dim)
+        self.user_mlp_2 = nn.Linear(args.embed_dim, args.embed_dim)
+        self.event_mlp_1 = nn.Linear(args.embed_dim * 3, args.embed_dim)
+        self.event_mlp_2 = nn.Linear(args.embed_dim, args.embed_dim)
+
+
+        self.fc1 = nn.Linear(args.embed_dim * 2, args.hidden_size)
         self.fc2 = nn.Linear(args.hidden_size, args.hidden_size)
         self.fc3 = nn.Linear(args.hidden_size, args.n_output)
 
@@ -26,9 +32,16 @@ class DNN(nn.Module):
         m_topic_embedding = torch.mean(m_topic_embedding, dim=1)
         g_topic_embedding = torch.mean(g_topic_embedding, dim=1)
 
+        user_concat = torch.cat((m_embedding, m_topic_embedding), dim=-1)
+        event_concat = torch.cat((org_embedding, g_embedding, g_topic_embedding), dim=-1)
+        user_embedding = F.relu(self.user_mlp_1(user_concat))
+        user_embedding = F.relu(self.user_mlp_2(user_embedding))
+        event_embedding = F.relu(self.event_mlp_1(event_concat))
+        event_embedding = F.relu(self.event_mlp_2(event_embedding))
+        concat_embedding = torch.cat((user_embedding, event_embedding), dim=-1)
 
-        concat_embed = torch.cat((org_embedding, g_embedding, m_embedding, m_topic_embedding, g_topic_embedding), dim=-1)
-        hidden = self.fc1(concat_embed)
+        # concat_embed = torch.cat((org_embedding, g_embedding, m_embedding, m_topic_embedding, g_topic_embedding), dim=-1)
+        hidden = self.fc1(concat_embedding)
         hidden = F.relu(hidden)
         hidden = self.fc2(hidden)
         hidden = F.relu(hidden)
--- train.py
+++ train.py
@@ -14,9 +14,10 @@ from tqdm import tqdm
 from torch.utils.data import DataLoader
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support, roc_auc_score
 
+
 def train():
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-    data_dict, topic_dict = dh.load_data() # data_dict, [group2topic, mem2topic]
+    data_dict, topic_dict = dh.load_data()  # data_dict, [group2topic, mem2topic]
 
     train_data, train_label, dev_data, dev_label, test_data, test_label = dh.data_split(data_dict, topic_dict)
     train_dataset = dh.Dataset(train_data, train_label)
@@ -24,9 +25,9 @@ def train():
     dev_dataset = dh.Dataset(dev_data, dev_label)
     dev_loader = DataLoader(dev_dataset, batch_size=128, shuffle=True)
 
-    lambda1 = lambda epoch : (epoch / args.warm_up_step) if epoch < args.warm_up_step else 0.5 * (
-                math.cos((epoch - args.warm_up_step) / (args.n_epoch * len(train_dataset) - args.warm_up_step) * math.pi) + 1)
-
+    lambda1 = lambda epoch: (epoch / args.warm_up_step) if epoch < args.warm_up_step else 0.5 * (
+            math.cos(
+                (epoch - args.warm_up_step) / (args.n_epoch * len(train_dataset) - args.warm_up_step) * math.pi) + 1)
 
     model = DNN(args).to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=args.init_lr)
@@ -52,8 +53,8 @@ def train():
 
             if global_step % args.report_step == 0:
                 logger.info('loss: {}, lr: {}, epoch: {}'.format(np.average(loss_deq).item(),
-                                                                     optimizer.param_groups[0]['lr'],
-                                                                     global_step/len(train_dataset)))
+                                                                 optimizer.param_groups[0]['lr'],
+                                                                 global_step / len(train_dataset)))
             if global_step % args.eval_step == 0:
                 model.eval()
                 eval_result = evaluation(model, data_loader=dev_loader, device=device)
@@ -63,19 +64,21 @@ def train():
                     best_f1 = eval_result['f1']
                 model.train()
 
+
 def evaluation(model, data_loader, device):
     def compute_metrics(preds, labels, loss):
         precision, recall, f1, _ = precision_recall_fscore_support(labels, np.round(preds).tolist(), average='binary')
         acc = accuracy_score(labels, np.round(preds).tolist())
         # auc = roc_auc_score(labels, preds, multi_class='ovo')
         return {
-            'loss' : np.average(loss),
+            'loss': np.average(loss),
             'accuracy': acc,
             'f1': f1,
             'precision': precision,
             'recall': recall,
             # 'auc':  auc,
         }
+
     accu_loss = []
     accu_pred = []
     accu_label = []
@@ -94,20 +97,21 @@ def evaluation(model, data_loader, device):
 
     return compute_metrics(accu_pred, accu_label, accu_loss)
 
+
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser()
-    argparser.add_argument('--task_name', type=str,default='test')
-    argparser.add_argument('--warm_up_step', type=int,default=100)
-    argparser.add_argument('--report_step', type=int,default=1000)
-    argparser.add_argument('--eval_step', type=int,default=3000)
-    argparser.add_argument('--n_epoch', type=int,default=10)
-    argparser.add_argument('--init_lr', type=float,default=5e-4)
-    argparser.add_argument('--batch_size', type=int,default=64)
-    argparser.add_argument('--embed_dim', type=int,default=32)
-    argparser.add_argument('--hidden_size', type=int,default=32)
+    argparser.add_argument('--task_name', type=str, default='test092349')
+    argparser.add_argument('--warm_up_step', type=int, default=100)
+    argparser.add_argument('--report_step', type=int, default=1000)
+    argparser.add_argument('--eval_step', type=int, default=3000)
+    argparser.add_argument('--n_epoch', type=int, default=10)
+    argparser.add_argument('--init_lr', type=float, default=1e-3)
+    argparser.add_argument('--batch_size', type=int, default=64)
+    argparser.add_argument('--embed_dim', type=int, default=32)
+    argparser.add_argument('--hidden_size', type=int, default=32)
     argparser.add_argument('--n_mem', type=int, default=73629)
     argparser.add_argument('--n_ene', type=int, default=55396)
-    argparser.add_argument('--n_group',type=int,default=482)
+    argparser.add_argument('--n_group', type=int, default=482)
     argparser.add_argument('--n_topic', type=int, default=17120)
     argparser.add_argument('--n_output', type=int, default=2)
     args = argparser.parse_args()
